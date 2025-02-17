f8ca8a48d61da236baba14cffc8a260931557403
Diego EFC <43099313+DiegoEmanuel@users.noreply.github.com>
2025-02-17 16:49:49 -0300

    fix: add fix to acess image from vercell

--- next.config.ts
+++ next.config.ts
@@ -35,6 +35,18 @@ const nextConfig = {
       destination: "http://34.205.99.179:3000/products",
     },
   ],
+  //essa configuação abaixo é para o nextjs permitir que o backend seja acessado pelo frontend mesmo o back estando sem SSL
+  headers: async () => [
+    {
+      source: '/api/:path*',
+      headers: [
+        { key: 'Access-Control-Allow-Credentials', value: 'true' },
+        { key: 'Access-Control-Allow-Origin', value: '*' },
+        { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
+        { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
+      ],
+    },
+  ],
 }
 
 module.exports = nextConfig 
