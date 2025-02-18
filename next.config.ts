339f7991f22992ef8ad6610cd91a2c5a8c5515f1
Diego EFC <43099313+DiegoEmanuel@users.noreply.github.com>
2025-02-17 22:13:05 -0300

    feat: Update image remote patterns for multiple environments

--- next.config.ts
+++ next.config.ts
@@ -2,28 +2,35 @@
 const nextConfig = {  
   images: {
     remotePatterns: [
-      // localhost
+      //localhost
       {
         protocol: 'http',
         hostname: 'localhost',
         port: '3000',
         pathname: '/uploads/products/**',
       },
-      // // render
-      // {
-      //   protocol: 'https',
-      //   hostname: 'teste-tecnico-syno-api.onrender.com',
-      //   pathname: '/uploads/products/**',
-      // },
-      // aws
+      //render
+      {
+        protocol: 'https',
+        hostname: 'teste-tecnico-syno-api.onrender.com',
+        port: '',
+        pathname: '/uploads/products/**',
+      },
+      //aws
       {
         protocol: 'http',
         hostname: '34.205.99.179',
         port: '3000',
         pathname: '/uploads/products/**',
+      },
+      //firebase
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/v0/b/syno-2bc76.firebasestorage.app/o/**',
       }
     ],
-    domains: ['teste-tecnico-syno-api.onrender.com']
   },
   rewrites: async () => [
     {
